83e3c9d89b97d28028e0e6179baeb5a50145edb4
Christien Wong <christienalwong@gmail.com>
2026-03-22 23:15:20 -0500

    Initial Commit

---
+++ Controllers/SearchViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ChristienWong-Lab4
+//
+//  Created by user286461 on 3/22/26.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
